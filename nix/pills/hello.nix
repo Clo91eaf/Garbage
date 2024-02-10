12d044f22e34e61db8f173e4dc83b22fd1a144e2
Clo91eaf <Clo91eaf@qq.com>
2024-02-11 02:02:01 +0800

    Add more common nix scripts.

--- nix/pills/hello.nix
+++ nix/pills/hello.nix
@@ -1,12 +1,8 @@
 let
   pkgs = import <nixpkgs> {};
-in
-  derivation {
+  mkDerivation = import ./autotools.nix pkgs;
+in 
+  mkDerivation {
     name = "hello";
-    builder = "${pkgs.bash}/bin/bash";
-    args = [ ./hello_builder.sh ];
-    inherit (pkgs) gnutar gzip gnumake gcc coreutils gawk gnused gnugrep;
-    bintools = pkgs.binutils.bintools;
     src = ./hello-2.12.1.tar.gz;
-    system = builtins.currentSystem;
   }
